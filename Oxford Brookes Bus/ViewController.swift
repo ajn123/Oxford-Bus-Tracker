277dba0aadc75d169e9c7351744613e3fba9b8a3
ajn123 <ajn123@vt.edu>
2015-04-20 15:15:01 +0100

    Initial Commit

---
+++ Oxford Brookes Bus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Oxford Brookes Bus
+//
+//  Created by AJ Norton on 4/20/15.
+//  Copyright (c) 2015 AJ Norton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Oxford Brookes BusTests/Oxford_Brookes_BusTests.swift
@@ -0,0 +1,36 @@
+//
+//  Oxford_Brookes_BusTests.swift
+//  Oxford Brookes BusTests
+//
+//  Created by AJ Norton on 4/20/15.
+//  Copyright (c) 2015 AJ Norton. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Oxford_Brookes_BusTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
