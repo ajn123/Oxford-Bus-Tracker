7a961d69765ac88376dbb5db81024be06891853f
ajn123 <ajn123@vt.edu>
2015-06-01 15:51:25 +0100

    added alert and custom view cell

---
+++ Oxford Brookes Bus/Alert.swift
@@ -0,0 +1,22 @@
+//
+//  Alert.swift
+//  Oxford Brookes Bus
+//
+//  Created by AJ Norton on 6/1/15.
+//  Copyright (c) 2015 AJ Norton. All rights reserved.
+//
+
+import UIKit
+
+
+class Alert {
+    
+    class func presentErrorSheet(message: String, view: UIViewController)
+    {
+        var ActionSheet =  UIAlertController(title: message, message: "hellos", preferredStyle: UIAlertControllerStyle.ActionSheet)
+        
+        ActionSheet.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Cancel, handler:nil))
+        
+        view.presentViewController(ActionSheet, animated: true, completion: nil)
+    }
+}
--- Oxford Brookes Bus/CustomRouteViewCell.swift
+++ Oxford Brookes Bus/CustomRouteViewCell.swift
@@ -65,25 +65,42 @@ class CustomRouteViewCell: UITableViewCell {
             
             var refreshAlert = UIAlertController(title: "Reminder", message: "Set a reminder for the bus.", preferredStyle: UIAlertControllerStyle.Alert)
             
-            refreshAlert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: { (action: UIAlertAction!) in
-                
-                let strTime = refreshAlert.textFields![0] as! UITextField
-                let time = strTime.text.toInt()!
-                Alarm.createReminder("Catch the Bus",
-                    timeInterval: NSDate(timeIntervalSinceNow: Double(time * 60)))
-            }))
-            
-            refreshAlert.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: { (action: UIAlertAction!) in
-                println("Handle Cancel Logic here")
-            }))
+            refreshAlert.addAction(UIAlertAction(title: "Cancel", style: .Cancel)
+                { action in
+                    println("Handle Cancel Logic here")
+                })
             
             
             refreshAlert.addTextFieldWithConfigurationHandler()
-            { textField -> Void in
-                textField.text = "\(self.timeInterval)"
-                textField.keyboardAppearance = UIKeyboardAppearance.Dark
-                textField.keyboardType = UIKeyboardType.NumberPad
+                { textField -> Void in
+                    textField.text = "\(self.timeInterval)"
+                    textField.keyboardAppearance = UIKeyboardAppearance.Dark
+                    textField.keyboardType = UIKeyboardType.NumberPad
             }
+            
+            refreshAlert.addAction(UIAlertAction(title: "Ok", style: .Default) { action in
+                let strTime = refreshAlert.textFields![0] as! UITextField
+                if( strTime.text != "")
+                {
+                    let time = strTime.text.toInt()!
+                    if(time < 300)
+                    {
+                        Alarm.createReminder("Catch the Bus",
+                        timeInterval: NSDate(timeIntervalSinceNow: Double(time * 60)))
+                    }
+                    else
+                    {
+                        Alert.presentErrorSheet("Select a time under 300 minutes", view: self.parentViewController!)
+                    }
+                }
+                else
+                {
+                    Alert.presentErrorSheet("Select a real time!", view: self.parentViewController!)
+                  
+                }
+            })
+            
+       
             
             self.parentViewController?.presentViewController(refreshAlert, animated: true, completion: nil)
             leadingSpaceConstraint.constant = 0
