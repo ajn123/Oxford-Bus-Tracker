355bf5b4613f34fb49e5a49ce0d90f9bc6d9e709
ajn123 <ajn123@vt.edu>
2015-07-08 20:52:03 -0400

    added hospital stop

--- bus_xml.rb
+++ bus_xml.rb
@@ -25,23 +25,23 @@ class CoreDataModel
 buses = ["U1", "U4", "U5", "U5X"] # U5 needs to be added
 
 buses.each do |bus|
-
 	xlsx = Roo::Excelx.new("#{bus}.xlsx")
 
-
-	dict  = { 
+	dict = 
+	{ 
 		"Brookes Uni stop B5" => [51.755474, -1.226263], "Headington Shops" => [51.759688, -1.212619],
-		"Harcourt Hill" => [51.7404539,-1.2914457], "Wheatley campus" => [51.7488942, -1.1265288], 
-		"Wheatley church" => [51.747191, -1.135268], "Castle Street" => [51.751495,-1.260925], 
-		"Speedwell Street" => [51.748428,-1.258112], "OXFORD High St Carfax" => [51.751985,-1.2580974],
-		"Frideswide Sq R8" => [51.752617, -1.267921], "Frideswide Sq R9" => [51.7526129,-1.2680492], 
-		"Sandhills A40" => [51.7631259,-1.1808569],     "Jack Straws Lane" => [51.762788, -1.235677],
-		"Brookes Uni stop B2" => [51.7558266,-1.2253118], "High St Turl St" => [51.7521907,-1.2563702],
+		"Harcourt Hill" => [51.7404539, -1.2914457], "Wheatley campus" => [51.7488942, -1.1265288], 
+		"Wheatley church" => [51.747191, -1.135268], "Castle Street" => [51.751495, -1.260925], 
+		"Speedwell Street" => [51.748428, -1.258112], "OXFORD High St Carfax" => [51.751985, -1.2580974],
+		"Frideswide Sq R8" => [51.752617, -1.267921], "Frideswide Sq R9" => [51.7526129, -1.2680492], 
+		"Sandhills A40" => [51.7631259, -1.1808569],     "Jack Straws Lane" => [51.762788, -1.235677],
+		"Brookes Uni stop B2" => [51.7558266, -1.2253118], "High St Turl St" => [51.7521907, -1.2563702],
 		"Paul Kent Hall" => [51.741643, -1.204229], "John Radcliffe Hospital" => [51.764599, -1.222692],
-		"Slade Park Hall" => [51.743682, -1.200077], "St Aldates" => [51.7511288,-1.2573163,],
+		"Slade Park Hall" => [51.743682, -1.200077], "St Aldates" => [51.7511288, -1.2573163,],
 		"Corner House" => [51.743700, -1.202181], "Crescent Hall" => [51.737698, -1.207406], 
-		"Gipsy Lane stop B7" => [51.754856,-1.222536], "Cowley The Swan" => [51.749238, -1.242079],
+		"Gipsy Lane stop B7" => [51.754856, -1.222536], "Cowley The Swan" => [51.749238, -1.242079],
 		"Cherwell Drive" => [51.770155, -1.235753], "Cowley Rd The Plain" => [51.749205, -1.242058],
+		"Gipsy Lane Stop B6" => [51.754625, -1.2223642], "Headley Way for JR Hosp" => [51.7639481, -1.2250515]
 	}
 
 	# 51.760456, -1.210038 = Headington Shops Wheatley Campus
